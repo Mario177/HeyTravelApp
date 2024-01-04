2eb515b9b502aede6f6b5a69f9471007ae4d9334
mario <ma455048@gmail>
2024-01-04 10:51:50 +0800

    Finish page3(AddNewPlace)'s setting!
    Page3(AddNewPlace) fragment!

--- app/src/main/java/com/example/heytravel/LanguageActivity.kt
+++ app/src/main/java/com/example/heytravel/LanguageActivity.kt
@@ -1,11 +1,19 @@
 package com.example.heytravel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LanguageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language)
+
+        val button = findViewById<Button>(R.id.btn_back)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
